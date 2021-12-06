4e8a89514213d4fba5ba336051a1735567edf2d5
Kristjan Siimson <k.siimson@sportradar.com>
2021-12-06 18:46:38 +0100

    d06

---
+++ d06/README.md
@@ -0,0 +1,31 @@
+# Day 6: Lanternfish
+
+## Run
+
+```
+cat assets/input.txt | cargo run
+```
+
+## Bench
+
+```
+cat assets/input.txt | cargo bench
+```
+
+## Log
+
+### Part 1
+
+This looks like it would have a mathematical solution, but it was easiest to just
+model programmatically.
+
+### Part 2
+
+Just increment the number of iterations.
+
+Local benchmark results:
+
+* parse: 705 ns
+* part 1: 21 μs
+* part 2: 126 μs
+ 
---
+++ d06/src/part2.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+type Glowfish = (u16, u16);
+
+pub fn solve(input: &Vec<u8>) -> usize {
+   let mut result = 0;
+   let mut cache = HashMap::new();
+   for fish in input {
+      result += count((*fish as u16, 256), &mut cache);
+   }
+   result
+}
+
+fn count(data: Glowfish, cache: &mut HashMap<Glowfish,usize>) -> usize {
+   if let Some(val) = cache.get(&data) {
+      return *val;
+   }
+   let mut result = 1;
+   if let Some(children) = populate(data) {
+      for child in children {
+         result += count(child, cache);
+      }
+   }
+   cache.insert(data, result);
+   result
+}
+
+fn populate(data: Glowfish) -> Option<Vec<Glowfish>> {
+   let (days_to_spawn, days_to_end) = data;
+   if days_to_end < days_to_spawn + 1 {
+      return None;
+   }
+   let mut result = Vec::new();
+   let mut days_to_end = days_to_end - days_to_spawn - 1;
+   result.push((8, days_to_end));
+   while days_to_end >= 7 {
+      days_to_end -= 7;
+      result.push((8, days_to_end));
+   }
+   Some(result)
+}
