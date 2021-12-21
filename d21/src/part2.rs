79e899d32f6dcdfebac1d6559e79e48baf386d32
Kristjan Siimson <k.siimson@sportradar.com>
2021-12-21 15:45:33 +0100

    d21 merge functions

--- d21/src/part2.rs
+++ d21/src/part2.rs
@@ -5,21 +5,21 @@ type Score = (usize, usize);
 type Position = (usize, usize);
 
 pub fn solve(input: &(usize, usize)) -> usize {
-    max(
-        get_win_paths_p1((0, 0), (input.0 - 1, input.1 - 1), &mut HashMap::new()),
-        get_win_paths_p2((0, 0), (input.0 - 1, input.1 - 1), &mut HashMap::new()),
-    )
+    let mut cache_1 = HashMap::new();
+    let result = count_universes((0, 0), (input.0 - 1, input.1 - 1), &mut cache_1);
+    max(result.0, result.1)
 }
 
-fn get_win_paths_p1(
+fn count_universes(
     score: Score,
     position: Position,
-    cache: &mut HashMap<(Score, Position), usize>,
-) -> usize {
+    cache: &mut HashMap<(Score, Position), (usize, usize)>,
+) -> (usize, usize) {
     if let Some(result) = cache.get(&(score, position)) {
         return *result;
     }
-    let mut result = 0;
+    let mut p1_universe_count = 0;
+    let mut p2_universe_count = 0;
     for p1_roll_first in 1..=3 {
         for p1_roll_second in 1..=3 {
             for p1_roll_third in 1..=3 {
@@ -27,7 +27,7 @@ fn get_win_paths_p1(
                     (position.0 + p1_roll_first + p1_roll_second + p1_roll_third) % 10;
                 let p1_score = score.0 + p1_position + 1;
                 if p1_score >= 21 {
-                    result += 1;
+                    p1_universe_count += 1;
                 } else {
                     for p2_roll_first in 1..=3 {
                         for p2_roll_second in 1..=3 {
@@ -37,54 +37,18 @@ fn get_win_paths_p1(
                                         % 10;
                                 let p2_score = score.1 + p2_position + 1;
                                 if p2_score < 21 {
-                                    result += get_win_paths_p1(
+                                    match count_universes(
                                         (p1_score, p2_score),
                                         (p1_position, p2_position),
                                         cache,
-                                    );
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    cache.insert((score, position), result);
-    result
-}
-
-fn get_win_paths_p2(
-    score: Score,
-    position: Position,
-    cache: &mut HashMap<(Score, Position), usize>,
-) -> usize {
-    if let Some(result) = cache.get(&(score, position)) {
-        return *result;
-    }
-    let mut result = 0;
-    for p1_roll_first in 1..=3 {
-        for p1_roll_second in 1..=3 {
-            for p1_roll_third in 1..=3 {
-                let p1_position =
-                    (position.0 + p1_roll_first + p1_roll_second + p1_roll_third) % 10;
-                let p1_score = score.0 + p1_position + 1;
-                if p1_score < 21 {
-                    for p2_roll_first in 1..=3 {
-                        for p2_roll_second in 1..=3 {
-                            for p2_roll_third in 1..=3 {
-                                let p2_position =
-                                    (position.1 + p2_roll_first + p2_roll_second + p2_roll_third)
-                                        % 10;
-                                let p2_score = score.1 + p2_position + 1;
-                                if p2_score >= 21 {
-                                    result += 1;
+                                    ) {
+                                        (p1, p2) => {
+                                            p1_universe_count += p1;
+                                            p2_universe_count += p2;
+                                        }
+                                    }
                                 } else {
-                                    result += get_win_paths_p2(
-                                        (p1_score, p2_score),
-                                        (p1_position, p2_position),
-                                        cache,
-                                    );
+                                    p2_universe_count += 1;
                                 }
                             }
                         }
@@ -93,6 +57,7 @@ fn get_win_paths_p2(
             }
         }
     }
-    cache.insert((score, position), result);
-    result
+    cache.insert((score, position), (p1_universe_count, p2_universe_count));
+
+    (p1_universe_count, p2_universe_count)
 }
